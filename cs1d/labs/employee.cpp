519601d3d195481b3231b99521ad95f5d71c48cd
Souzen Khan <sawsankhan303@gmail.com>
2023-01-25 13:23:25 -0800

    changed employee names

--- cs1d/labs/employee.cpp
+++ cs1d/labs/employee.cpp
@@ -8,10 +8,10 @@ using namespace std;
 int main() 
 { 
  personType newPerson; 
- partTimeEmployee newEmployee("John","Smith",7.50,56); 
+ partTimeEmployee newEmployee("Sophie","Smith",7.50,56); 
  partTimeEmployee employee; 
        newEmployee.print(); 
- employee.setNameRateHours("Rachel", "Moore",9.75, 45); 
+ employee.setNameRateHours("Souzen", "Khan",9.75, 45); 
  employee.print(); 
  
  return 0; 
