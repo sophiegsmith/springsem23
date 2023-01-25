d20a37a3d4f131223f66ea902ee4df7c062a3712
ssmith2020 <sophiagsmith410@gmail.com>
2023-01-24 17:10:59 -0800

    also part b of the lab

---
+++ cs1d/labs/inherit2i.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <string> 
+#include "inherit2.h" 
+ 
+using namespace std; 
+ 
+ 
+void personType::print() const 
+{ 
+ cout<<firstName<<" "<<lastName; 
+} 
+ 
+void personType::setName(string first, string last) 
+{ 
+ firstName = first; 
+ lastName = last; 
+} 
+ 
+void personType::getName(string& first, string& last) 
+{ 
+ first = firstName; 
+ last = lastName; 
+} 
+ 
+ //constructor with parameters 
+personType::personType(string first, string last) 
+ 
+{ 
+ firstName = first; 
+ lastName = last; 
+} 
