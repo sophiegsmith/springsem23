113ab4329ce0831845126a72680781d660034795
ssmith2020 <sophiagsmith410@gmail.com>
2023-01-26 14:27:29 -0800

    starting cs1d

---
+++ cs1d/assignments/hw1.cpp
@@ -0,0 +1,26 @@
+/***********************************************************************************************************
+ * Programmer: Sophie Smith
+ * Instructor Name: Lebowitz
+ * Section: MW 2-4:20 pm
+ * Date: 1/26/23
+ * Description:
+ * This program takes a word and uses a recursive function to see if the word inputted is a Palindrome. 
+ * It ignores white spaces, capital letters and punctuation.
+ ***********************************************************************************************************/
+#include<iostream>
+
+using namespace std;
+
+//function prototype
+
+//main function
+int main()
+{
+
+    return 0;
+} //end of main
+
+/***********************************************************************************************************
+ * Description: Outputs the word entered and whether it is a palindrome or not.
+ * * Output:
+ ***********************************************************************************************************/
